4b308b473deb8f99c137cb80870d1c4f6a01f36a
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-20 10:57:45 +0000

    vmskeifjf

---
+++ script.js
@@ -0,0 +1,16 @@
+const wackyForecasts = [
+  "Cats and Dogs Raining",
+  "The Sun Exploding",
+  "Gravity Reversal Day",
+  "Technicolor Tornado",
+  "Spaghetti Snowstorm",
+  "Marshmallow Meteor Shower",
+  "Bubble Gum Blizzard",
+  "Disco Ball Sun",
+  "Pizza scented Fog",
+  "Talking Cloud Convention"
+];
+
+function generateForecast() {
+  const forecastElement = document.getElementById('forecast');
+  const randomIndex 
